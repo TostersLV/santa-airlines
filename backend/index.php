4b3ccc46781a529bbeccdfe270935e1174ae86d7
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-11-20 10:31:34 +0200

    nomainiju 5

--- backend/index.php
+++ backend/index.php
@@ -8,7 +8,7 @@ function addFour($x){
     $rezultats = $x + 4;
     echo "<br>  $rezultats";
 }
-addFour(4);
+addFour(5);
 
 $manaLidmasina = new Aircraft("Airbus", "A220-300", 120, 850);
 var_dump($manaLidmasina);
